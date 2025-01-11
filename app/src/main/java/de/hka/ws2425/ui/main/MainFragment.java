088b9312b6eefcce1e6c1cc96c526658cdffa709
Moritz Gimpel-Henning <gimo1012@h-ka.de>
2025-01-11 18:30:26 +0100

    Startseite integrierte

--- app/src/main/java/de/hka/ws2425/ui/main/MainFragment.java
+++ app/src/main/java/de/hka/ws2425/ui/main/MainFragment.java
@@ -1,24 +1,13 @@
 package de.hka.ws2425.ui.main;
 
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import de.hka.ws2425.api.RandomJoke;
-import de.hka.ws2425.ui.map.MapFragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import de.hka.ws2425.R;
 
 public class MainFragment extends Fragment {
@@ -33,7 +22,6 @@ public class MainFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
     }
 
     @Nullable
@@ -41,53 +29,5 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        /*TextView txtMessage = this.getView().findViewById(R.id.txt_message);
-        txtMessage.setText("This is a stupid way to set a text ....");*/
-
-        Button btnDoSomething = this.getView().findViewById(R.id.btn_do_something);
-        btnDoSomething.setOnClickListener((view) -> {
-            FragmentTransaction fragmentTransaction = this.getActivity().getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.addToBackStack("MainFragment");
-            fragmentTransaction.replace(R.id.container, new MapFragment());
-            fragmentTransaction.commit();
-        });
-
-        TextView txtMessage = this.getView().findViewById(R.id.txt_message);
-
-        mViewModel.getResult().observe(this.getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                txtMessage.setText(s);
-            }
-        });
-
-        // WebService
-        Button btnGetRandomJoke = this.getView().findViewById(R.id.btn_get_random_joke);
-        btnGetRandomJoke.setOnClickListener(view -> this.mViewModel.requestRandomJoke());
-
-        TextView txtJokeSetup = this.getView().findViewById(R.id.txt_joke_setup);
-        TextView txtJokePunchline = this.getView().findViewById(R.id.txt_joke_punchline);
-
-        mViewModel.getRandomJokeResponse().observe(this.getViewLifecycleOwner(), new Observer<RandomJoke>() {
-            @Override
-            public void onChanged(RandomJoke randomJoke) {
-                txtJokeSetup.setText(randomJoke.getSetup());
-                txtJokePunchline.setText(randomJoke.getPunchline());
-            }
-        });
-
-        // MQTT-Broker
-        Button btnSendMqttMessage = this.getView().findViewById(R.id.btn_send_mqtt_message);
-        EditText edtMqttMessage = this.getView().findViewById(R.id.edt_mqtt_message);
-
-        btnSendMqttMessage.setOnClickListener(view -> {
-            this.mViewModel.sendMqttMessage("hka/test/app/message", edtMqttMessage.getText().toString());
-        });
     }
 }
